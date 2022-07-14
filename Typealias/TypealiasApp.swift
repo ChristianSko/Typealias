32b7f15f32d3939ec7bb4bc99749b6396bd3c8fe
Christian Skorobogatow <christiansko@icloud.com>
2022-07-14 23:49:23 +0200

    Initial Commit

---
+++ Typealias/TypealiasApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypealiasApp.swift
+//  Typealias
+//
+//  Created by Christian Skorobogatow on 14/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TypealiasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
